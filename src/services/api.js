479979d9f3c96b892e3def0cdf3b2003dde48852
s0nren <piard.hugo2004@gmail.com>
2025-04-28 10:34:04 +0200

    fix : correction of the code in relation to the remarks

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiBaseUrl =  'http://localhost:8081'; 
+const apiBaseUrl = process.env.FRONT_BASE_URL; 
 const api = axios.create({
     baseURL: apiBaseUrl,
     headers: {
@@ -8,22 +8,11 @@ const api = axios.create({
     }
 });
 
-//add request interceptors for authentification tokens 
-/*api.interceptors.request.use(
-    config => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers['Authorization'] = 'Bearer ${token]';
-        }
-        return config;
-    },
-    error => Promise.reject(error)
-);*/
 export const movieService = {
     getAll: () => api.get('/movies'),
     getById: (id) => api.get(`/movies/ ${id}`),
     create: (data) => api.post('/movies', data),
     update: (id, data) => api.put(`/movies/ ${id} `, data),
-    delete: (id) => api.delete(`/movie/ ${id} `)
+    delete: (id) => api.delete(`/movies/ ${id} `)
 };
 export default api;
