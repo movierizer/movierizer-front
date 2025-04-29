da2ec2c8479c7a97100275fd25760e83c2bf0964
s0nren <piard.hugo2004@gmail.com>
2025-04-29 19:12:03 +0200

    feat : add of some little functionnalities to the form

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import { movieService } from '../services/api';
-import MovieForm from "./MovieForm";
 
 const MovieForm = () => {
     const [title, setTitle] = useState([]);
     const [grade, setGrade] = useState([]);
     const [description, setDescription] = useState([]);
     const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [successMessage, setSuccessMessage] = useState(null);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -15,59 +16,72 @@ const MovieForm = () => {
             setError('Title is required');
             return;
         }
-        
+
         const newMovie = {
             title,
             grade,
             description,
         };
-
-        const reponse = await movieService.create(newMovie);
-       
-        console.log('Film ajouté:', newMovie);
-
-        setTitle('');
-        setDescription('');
-        setGrade(0);
-        setMovies([...movies, newMovie]);
+        
+        try{
+          const reponse = await movieService.create(newMovie);
+          setLoading(false);
+          console.log('Movie added :', newMovie);
+          setSuccessMessage(newMovie.title + ' added successfully');
+          setTitle('');
+          setDescription('');
+          setGrade(0);
+          setTimeout(() => {setSuccessMessage(null);}, 3000); 
+        } catch (err){
+          setError("Failed to add movie");
+          setLoading(false);
+          console.log(err);
+        }
     };
 
+    if (loading) return <div>Loading...</div>
+    if (error) return <div className="alert alert-danger">{error}</div>
+
     return(
+      <div>
+        {error && <div className="error-message">{error}</div>}
+        {successMessage && <div className="success-message">{successMessage}</div>}
         <form onSubmit={handleSubmit}>
-        <div>
-            <label htmlFor="title"> Add a movie</label> 
-            <input 
-            type="text" 
-            name="tiltle"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            required />
-        </div>
-        <div>
-          <label>
-            grade:
-            <input 
-            type="number" 
-            name="grade" 
-            min={0} 
-            max={100}
-            value={grade}
-            onChange={(e) => setGrade(e.target.value)} />  
-          </label>
-        </div>
-        <div>
-          <label>
-            description:
-            <input 
-            type="text" 
-            name="description"
-            maxLength="500"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)} />
-          </label>
-        </div>
-        <input type="submit" value="add" />
-      </form>
+          <div>
+              <label htmlFor="title"> Add a movie</label> 
+              <input 
+              type="text" 
+              name="tiltle"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              required />
+          </div>
+          <div>
+            <label>
+              grade:
+              <input 
+              type="number" 
+              name="grade" 
+              min={0} 
+              max={100}
+              value={grade}
+              onChange={(e) => setGrade(e.target.value)} />  
+            </label>
+          </div>
+          <div>
+            <label>
+              description:
+              <input 
+              type="text" 
+              name="description"
+              maxLength="500"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)} />
+            </label>
+          </div>
+          <input type="submit" value="add" />
+        </form>
+      </div>
     )
 
 }
