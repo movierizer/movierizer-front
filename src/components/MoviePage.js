6f9bce3e6745d9cd174c268c8caa9a52ad2bc890
s0nren <piard.hugo2004@gmail.com>
2025-05-02 17:25:08 +0200

    fix : correction of  grammar errors

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -51,11 +51,11 @@ export default function MoviePage() {
                 <div className="col-md-8">
                 <div className="card-body">
                     <h2 className="card-title">{movie.title}</h2>
-                    <h6 className="card-subtitle mb-2 text-muted">{movie.originalTitle || 'No original title'}</h6>
-                    <p className="card-text mt-3">{movie.description || 'No description available.'}</p>
+                    <h6 className="card-subtitle mb-2 text-muted">{movie.original_title}</h6>
+                    <p className="card-text mt-3">{movie.overview}</p>
                     <ul className="list-group list-group-flush mt-4">
                     <li className="list-group-item"><strong>Grade:</strong> {movie.grade}</li>
-                    <li className="list-group-item"><strong>Release Date:</strong> {movie.releaseDate || 'N/A'}</li>
+                    <li className="list-group-item"><strong>Release Date:</strong> {movie.release_date}</li>
                     <li className="list-group-item"><strong>Runtime:</strong> {movie.runtime} minutes</li>
                     <li className="list-group-item"><strong>Budget:</strong> ${movie.budget.toLocaleString()}</li>
                     <li className="list-group-item"><strong>Revenue:</strong> ${movie.revenue.toLocaleString()}</li>
