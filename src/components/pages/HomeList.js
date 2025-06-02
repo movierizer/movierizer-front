c1c4ae6c123e0909b9972342b0e0aca413555002
s0nren <piard.hugo2004@gmail.com>
2025-06-02 13:05:44 +0000

    feat : restructure the home page

---
+++ src/components/pages/HomeList.js
@@ -0,0 +1,6 @@
+import { useEffect } from "react";
+
+const HomeList = () => {
+}
+
+export default HomeList
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,8 +1,90 @@
-/** This component is used to display the home page of the application where is associated with the url "/" */
+import movies from  "../../data/movies.json"
+import './HomePage.css';
+
+
 const HomePage = () => {
+
+    const randomMovie = movies[Math.floor(Math.random() * movies.length)];
+
+
+    const formatDate = (dateStr) => {
+        if (!dateStr) return 'Date inconnue';
+        const date = new Date(dateStr);
+        return date.toLocaleString('fr-FR', {
+        year: 'numeric',
+        });
+    };
+
     return (
-        <div>
-            <h1 className="display-1 text-white bg-dark position-absolute top-50 start-50 translate-middle">Movierizer Home Page</h1>
+        <div className="container mt-5" style={{ Width: '1296px' }}>
+            <div className=" backdrop-wrapper position-relative d-inline-block">
+                <img
+                src={randomMovie.backdrop_path}
+                alt={randomMovie.original_title}
+                className="backdrop-image"
+                />
+                <div className="backdrop-horizontal-fade" />
+                <div className="backdrop-top-fade" />
+                <div className="backdrop-bottom-fade" />
+
+                <div
+                    style={{
+                        position: 'absolute',
+                        bottom: '30px',
+                        left: '30px',
+                        right: '30px',
+                        display: 'flex',
+                        alignItems: 'flex-end',
+                        gap: '20px', 
+                        color: 'white',
+                        zIndex: 2}}>
+
+                    <img
+                    src={randomMovie.poster_path} 
+                    alt="poster"
+                    style={{
+                        position: 'absolute',
+                        bottom: '30px',  
+                        left: '30px',
+                        width: '200px',  
+                        height: 'auto',
+                        boxShadow: '0 4px 15px rgba(0,0,0,0.6)',
+                        borderRadius: '8px',
+                        transition: 'transform 0.3s ease',
+                        cursor: 'pointer',
+                        flexShrink: 0
+                    }}
+                    onMouseEnter={e => (e.currentTarget.style.transform = 'scale(1.05)')}
+                    onMouseLeave={e => (e.currentTarget.style.transform = 'scale(1)')}
+                    />
+
+                    <div
+                        style={{
+                        padding: '20px',
+                        backgroundColor: 'rgba(0, 0, 0, 0.1)',
+                        backdropFilter: 'blur(10px)',                
+                        WebkitBackdropFilter: 'blur(10px)',
+                        borderRadius: '15px',
+                        marginLeft: '250px',
+                        maxWidth: '800px',
+                        flexGrow: 1}}
+                    >
+                        <h1 style={{ marginBottom: '0px' }}>{randomMovie.original_title}</h1>
+                        <p style={{ marginBottom: '5px' }}>
+                            <strong> {randomMovie.director_name} </strong> 
+                        </p>
+                        <p style={{ marginBottom: '5px' }}>
+                            <strong>{formatDate(randomMovie.release_date)} </strong> 
+                        </p>
+
+                        <p style={{ marginTop: '10px' }}>
+                        {randomMovie.overview.length > 200
+                            ? randomMovie.overview.slice(0, 200) + '...'
+                            : randomMovie.overview}
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
